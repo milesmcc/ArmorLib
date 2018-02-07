787674ed4cce89fd7e7ce17702a40fd61ba190d3
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-07 13:31:54 -0500

    implement non-abstract data management (no longer hybrid)

--- src/binary_object.rs
+++ src/binary_object.rs
@@ -1,7 +1,11 @@
-use std::io::Read;
-
-#[derive(Copy)]
 pub struct BinaryObject {
     pub file_name: Option<String>,
-    pub data: Read
+    pub data: Vec<u8>
+}
+
+fn from(data: Vec<u8>) -> BinaryObject {
+    BinaryObject {
+        file_name: None,
+        data: data
+    }
 }
--- src/preprocessors/filetype.rs
+++ src/preprocessors/filetype.rs
@@ -11,6 +11,7 @@ pub enum Encoding {
     Utf8,
     Utf16,
     Utf32,
+    NoData,
     Binary
 }
 
@@ -26,11 +27,23 @@ impl Encoding {
     }
 
     pub fn determine_encoding(binary_object: &BinaryObject) -> Encoding {
-        let data: &Read = binary_object.clone().data;
-        let magic_bytes: &mut [u8] = &mut [8];
-        data.
+        if binary_object.data.iter().all(|x| *x < 128_u8) {
+            return Encoding::Ascii;
+        }
 
-        // TODO: how to read data if Read trait requires mutability?
+        if &binary_object.data.len() == &0_usize {
+            return Encoding::NoData;
+        }
+
+        let data: &[u8] = &binary_object.data.as_slice();
+
+        if data[0..4] == [0xF_b, 0xF_b, 0xF_b, 0xE_b] {
+            return Encoding::Utf16;
+        } else if data[0..6] == [0xE_b, 0xF_F, 0xB_b, 0xB_b, 0xB_b, 0xF_b] {
+            return Encoding::Utf8;
+        } else if data[0..8] == [0xF_b, 0xF_b, 0xF_b, 0xE_b, 0x0_b, 0x0_b, 0x0_b, 0x0_b] {
+            return Encoding::Utf32;
+        }
 
         Encoding::Binary
     }
@@ -38,9 +51,7 @@ impl Encoding {
 
 fn determine_file_types(binary_object: &BinaryObject) -> Vec<String> {
     // first, we must determine whether the file is a text-based file or a binary
-    let data: &Read = &binary_object.data;
-    let magic_bytes: &mut [u8] = &mut [8];
-    data.read_exact(magic_bytes);
+    let magic_bytes: &[u8] = &binary_object.data.as_slice()[0..8];
 
     let encoding = Encoding::determine_encoding(binary_object);
 
@@ -61,5 +72,43 @@ impl Preprocessor for FiletypePrepreprocessor {
 
     fn name() -> &'static str {
         "filetype"
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_determine_encoding() {
+        // test utf32
+        {
+            let binary_object_utf32 = BinaryObject::from(vec![0xF_u8, 0xF_u8, 0xF_u8, 0xE_u8, 0x0_u8, 0x0_u8, 0x0_u8,
+                                                              0x0_u8, 0x0_u8, 0x0_u8, 0x0_u8, 0x0_u8, 0x0_u8, 0x0_u8,
+                                                              0x0_u8, 0x0_u8, 0x0_u8, 0x0_u8, 0x0_u8, 0x0_u8, 0x0_u8]);
+            assert_eq!(Encoding::determine_encoding(binary_object_utf32), Encoding::Utf32);
+        }
+
+        // test no data
+        {
+            let binary_object_no_data = BinaryObject::from(vec![]);
+            assert_eq!(Encoding::determine_encoding(binary_object_no_data), Encoding::NoData);
+        }
+
+        // test ascii
+        {
+            let binary_object_ascii = BinaryObject::from(vec![98_u8, 86_u8, 98_u8, 86_u8, 98_u8, 86_u8, 98_u8, 86_u8]);
+            assert_eq!(Encoding::determine_encoding(binary_object_ascii), Encoding::Ascii);
+        }
+
+        // test utf8
+        {
+            let binary_object_utf8 = BinaryObject::from(vec![0xE_b, 0xF_F, 0xB_b, 0xB_b, 0xB_b, 0xF_b, 0x0_b]);
+            assert_eq!(Encoding::determine_encoding(binary_object_utf8), Encoding::Utf8);
+        }
+
+        // test utf16
+        {
+            let binary_object_utf16 = BinaryObject::from(vec![0xF_b, 0xF_b, 0xF_b, 0xE_b, 0x0_b, 0x0_b]);
+            assert_eq!(Encoding::determine_encoding(binary_object_utf16), Encoding::Utf16);
+        }
     }
 }
