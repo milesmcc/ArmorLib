577baa9ac31779feb285cad6695755d791f3b713
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-11 03:14:41 -0500

    Add future imports to coordinator

--- src/coordinator.rs
+++ src/coordinator.rs
@@ -1,9 +1,10 @@
 use scan_result::ScanResult;
 use binary_object::BinaryObject;
 use errors::ProcessingError;
-use preprocessor::{Preprocessor, PreprocessorObject};
-
-use preprocessors::{filetype, text};
+use preprocessor::Preprocessor;
+use scan_module::ScanModule;
+use preprocessors;
+use scan_modules;
 
 /// Process the given `BinaryObject` and return a `ScanResult`
 pub fn process(binary_object: &BinaryObject) -> Result<ScanResult, ProcessingError> {
