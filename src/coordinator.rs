df46e45178043c5ce4a474b32ee98cd4d47167d8
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-10 20:20:31 -0500

    Standardize coordinator

--- src/coordinator.rs
+++ src/coordinator.rs
@@ -1,11 +1,11 @@
 use scan_result::ScanResult;
 use binary_object::BinaryObject;
 use errors::ProcessingError;
-use std::io::Read;
+use preprocessor::{Preprocessor, PreprocessorObject};
+
+use preprocessors::{filetype, text};
 
 /// Process the given `BinaryObject` and return a `ScanResult`
-pub fn process<T: Read>(binary_object: &BinaryObject) -> Result<ScanResult, ProcessingError> {
-    Err(ProcessingError::UnknownProcessingError(String::from(
-        "not implemented",
-    )))
+pub fn process(binary_object: &BinaryObject) -> Result<ScanResult, ProcessingError> {
+    unimplemented!();
 }
