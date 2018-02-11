9f1b67d221f5b9fc90fbbcb72ef647078f80602e
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-10 20:22:25 -0500

    Make Preprocessors sized

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -1,7 +1,19 @@
 use std::collections::HashMap;
+
 use binary_object::BinaryObject;
 
-pub trait Preprocessor {
+pub trait Preprocessor: Sized {
     fn process(binary_object: &BinaryObject) -> HashMap<String, String>;
     fn name() -> &'static str;
+}
+
+pub struct PreprocessorObject;
+
+impl Preprocessor for PreprocessorObject {
+    fn process(binary_object: &BinaryObject) -> HashMap<String, String> {
+        unimplemented!();
+    }
+    fn name() -> &'static str {
+        unimplemented!();
+    }
 }
