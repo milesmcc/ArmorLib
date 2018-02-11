cd1d4b0d460cc55fc83d4579944c69799f32199d
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-11 03:16:53 -0500

    mostly implement scan_modules

--- src/scan_modules/mod.rs
+++ src/scan_modules/mod.rs
@@ -1,1 +1,26 @@
+use binary_object::BinaryObject;
+use scan_module::ScanModule;
+use scan_result::ScanResult;
+use scan_report::ScanReport;
+
+// List preprocessors here
 pub mod strings;
+
+pub fn scan_modules() -> Vec<Box<ScanModule>> {
+    vec![
+        Box::new(strings::StringsScanModule {}),
+        // ...and add additional preprocessors here
+    ]
+}
+
+pub fn process(scan_modules: Vec<Box<ScanModule>>, binary_object: &BinaryObject) -> ScanResult {
+    let scan_reports: Vec<ScanReport> = Vec::new();
+
+    for sm in scan_modules {
+        // TODO: run all preprocessors
+    }
+
+    ScanResult {
+        reports: scan_reports,
+    }
+}
