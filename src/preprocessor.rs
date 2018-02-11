001c7bd830b505ef75f3b9f9f334ce4cd7b85c8e
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-11 03:14:59 -0500

    Make preprocessors object-able

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -2,18 +2,7 @@ use std::collections::HashMap;
 
 use binary_object::BinaryObject;
 
-pub trait Preprocessor: Sized {
-    fn process(binary_object: &BinaryObject) -> HashMap<String, String>;
-    fn name() -> &'static str;
-}
-
-pub struct PreprocessorObject;
-
-impl Preprocessor for PreprocessorObject {
-    fn process(binary_object: &BinaryObject) -> HashMap<String, String> {
-        unimplemented!();
-    }
-    fn name() -> &'static str {
-        unimplemented!();
-    }
+pub trait Preprocessor {
+    fn process(binary_object: &BinaryObject) -> HashMap<String, String> where Self:Sized;
+    fn name() -> &'static str where Self:Sized;
 }
