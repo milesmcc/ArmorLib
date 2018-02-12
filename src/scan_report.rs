5c0748f48486524adb20a514edfee0d51431a16f
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-11 19:43:27 -0500

    Add error option to scan_report

--- src/scan_report.rs
+++ src/scan_report.rs
@@ -1,7 +1,9 @@
 use finding::Finding;
+use errors::ProcessingError;
 
 #[derive(Debug)]
 pub struct ScanReport {
     pub module_name: String,
-    pub findings: Vec<Finding>,
+    pub findings: Option<Vec<Finding>>,
+    pub error: Option<ProcessingError>,
 }
