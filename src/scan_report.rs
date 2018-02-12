a1d92e69cdea0e9783e1960cdb815d9fedac2960
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-11 23:37:03 -0500

    create module info field

--- src/scan_report.rs
+++ src/scan_report.rs
@@ -3,7 +3,8 @@ use errors::ProcessingError;
 
 #[derive(Debug)]
 pub struct ScanReport {
-    pub module_name: String,
+    /// The module's name and description.
+    pub module_info: (String, String),
     pub findings: Option<Vec<Finding>>,
     pub error: Option<ProcessingError>,
 }
