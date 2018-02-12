198588922307ce44aeefc012afc30982228112eb
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-11 23:36:13 -0500

    rename file_name to file_path

--- src/binary_object.rs
+++ src/binary_object.rs
@@ -1,12 +1,12 @@
 pub struct BinaryObject {
-    pub file_name: Option<String>,
+    pub file_path: Option<String>,
     pub data: Vec<u8>,
 }
 
 impl From<Vec<u8>> for BinaryObject {
     fn from(data: Vec<u8>) -> BinaryObject {
         BinaryObject {
-            file_name: None,
+            file_path: None,
             data: data,
         }
     }
