f25788048ebe9f3b0d5bf9b3b6219be44d747df3
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-19 16:47:29 -0500

    Add `scan_report` documentation.

--- src/scan_report.rs
+++ src/scan_report.rs
@@ -1,10 +1,21 @@
+//! This is a module that defines the `ScanReport`. The `ScanReport` is also available in the
+//! default namespace (accessible under `armorlib::ScanReport`).
+
 use finding::Finding;
 use errors::ArmorlibError;
 
+/// A `ScanReport` details the findings of a `ScanModule`, and includes various 'biographical'
+/// information about the scan module.
 #[derive(Debug)]
 pub struct ScanReport {
     /// The module's name and description.
     pub module_info: (String, String),
+
+    /// The findings of the scan module (see `ArmorLib::Finding`), if applicable. This may be
+    /// present regardless of the content of `error`.
     pub findings: Option<Vec<Finding>>,
+
+    /// The error created by the scan module, if applicable. This may be present regardless of the
+    /// content of `findings`.
     pub error: Option<ArmorlibError>,
 }
