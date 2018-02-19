d53566abea0ba4044636f02a3a7394beab489b01
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-19 16:35:11 -0500

    Add `scan_result` documentation

--- src/scan_result.rs
+++ src/scan_result.rs
@@ -1,5 +1,10 @@
+//! A `ScanResult` is a the "final product" when ArmorLib processes a `BinaryObject`. It is a
+//! collection (`Vec`) of `ScanReport`s, each of which was created by a `ScanModule`.
+
 use scan_report::ScanReport;
 
+/// A struct that represents a scan result. It is a wrapper for `ScanReport`s created by
+/// `ScanModule`s.
 #[derive(Debug)]
 pub struct ScanResult {
     pub reports: Vec<ScanReport>,
