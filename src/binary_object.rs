c2a03f4420cb26ddfa3bf4b6693bb2f637de652c
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-20 22:22:45 -0500

    Add documentation for `binary_object`

--- src/binary_object.rs
+++ src/binary_object.rs
@@ -1,8 +1,18 @@
+//! This module defines the BinaryObject, which represents a piece of unstructured binary data
+//! of any type.
+
 use std::fs::File;
 use std::io::Read;
 
+/// A struct that represents an unstructured piece of binary data in the form of a `Vec<u8>`.
+/// It also includes the option of having a `String` to designate the path.
 pub struct BinaryObject {
+    /// The path on the host operating system where this `BinaryObject` resides. Because this
+    /// field is not always applicible (for example, binary objects which exist only in a
+    /// database), the possibility of absence is represented in the `Option`.
     pub file_path: Option<String>,
+
+    /// A `Vec<u8>` of the binary object's data.
     pub data: Vec<u8>,
 }
 
