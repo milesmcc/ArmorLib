b464077ce28aef77f53f9ef3ae8c482cd855bb88
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-20 20:34:38 -0500

    implement From<File> for BinaryObject

--- src/binary_object.rs
+++ src/binary_object.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+
 pub struct BinaryObject {
     pub file_path: Option<String>,
     pub data: Vec<u8>,
@@ -9,5 +12,13 @@ impl From<Vec<u8>> for BinaryObject {
             file_path: None,
             data: data,
         }
+    }
+}
+
+impl From<File> for BinaryObject {
+    fn from(mut file: File) -> BinaryObject {
+        let mut data_vec: Vec<u8> = Vec::new();
+        file.read_to_end(&mut data_vec);
+        BinaryObject::from(data_vec)
     }
 }
