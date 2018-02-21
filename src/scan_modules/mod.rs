0d81f0a13b0066367c96c743966ca7932bddade5
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-20 22:32:11 -0500

    Add documentation to `scan_modules`

--- src/scan_modules/mod.rs
+++ src/scan_modules/mod.rs
@@ -1,3 +1,9 @@
+//! This module defines and manages the default scan modules available to ArmorLib.
+//!
+//! To contribute a new `ScanModule`, you must:
+//!     1. publicly import it into this module via `pub mod`.
+//!     2. instantiate it in `make_default_scan_modules()`.
+
 use scan_object::ScanObject;
 use scan_module::ScanModule;
 use scan_result::ScanResult;
@@ -7,6 +13,15 @@ use scan_report::ScanReport;
 pub mod strings;
 pub mod unicode_watermark;
 
+/// Create a `Vec<Box<ScanModule>>` of the core scan modules available to ArmorLib. This will
+/// instantiate the scan modules.
+///
+/// # Examples
+///
+/// ```rust
+/// use armorlib::scan_modules;
+/// let all_default_scan_modules = scan_modules::make_default_scan_modules();
+/// ```
 pub fn make_default_scan_modules() -> Vec<Box<ScanModule>> {
     vec![
         Box::new(strings::StringsScanModule {}),
@@ -15,6 +30,19 @@ pub fn make_default_scan_modules() -> Vec<Box<ScanModule>> {
     ]
 }
 
+/// Process the given `Vec<Box<ScanModule>>` on the given `ScanObject` and return a ScanResult.
+/// While concurrency is not yet available in ArmorLib, it will be implemented in this function,
+/// if anywhere. Be sure that the given `ScanObject` has the necessary metadata for the
+/// scan modules; no preprocessor checking is done here.
+///
+/// In _nearly all_ cases, it is better to perform `File.process()`, `Vec<u8>.process()`, or even
+/// `coordinator::process()` than to use this function. The previous functions will make sure
+/// that everything is set up properly; using this function alone will require you to manage
+/// the preprocessors yourself.
+///
+/// # Arguments
+/// * `scan_modules`: a `Vec<Box<ScanModule>>` of the scan modules to be run.
+/// * `scan_object`: a reference to a `ScanObject` with all necessary metadata present.
 pub fn process(scan_modules: Vec<Box<ScanModule>>, scan_object: &ScanObject) -> ScanResult {
     let mut scan_reports: Vec<ScanReport> = Vec::new();
 
