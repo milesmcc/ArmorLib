1101a50b5469664c6403500a561b82e2af8ac646
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-20 22:36:05 -0500

    Add documentation to `preprocessors`

--- src/preprocessors/mod.rs
+++ src/preprocessors/mod.rs
@@ -1,3 +1,9 @@
+//! This module defines and manages the default preprocessors available to ArmorLib.
+//!
+//! To contribute a new `Preprocessor`, you must:
+//!     1. publicly import it into this module via `pub mod`.
+//!     2. instantiate it in `make_default_preprocessors()`.
+
 use std::collections::HashMap;
 
 use binary_object::BinaryObject;
@@ -8,6 +14,15 @@ pub mod filetype;
 pub mod text;
 pub mod hex;
 
+/// Create a `Vec<Box<Preprocessor>>` of the core preprocessors available to ArmorLib. This will
+/// instantiate the preprocessors.
+///
+/// # Examples
+///
+/// ```rust
+/// use armorlib::preprocessors;
+/// let all_default_preprocessors = preprocessors::make_default_preprocessors();
+/// ```
 pub fn make_default_preprocessors() -> Vec<Box<Preprocessor>> {
     vec![
         Box::new(filetype::FiletypePrepreprocessor {}),
@@ -17,6 +32,19 @@ pub fn make_default_preprocessors() -> Vec<Box<Preprocessor>> {
     ]
 }
 
+/// Process the given `Vec<Box<Preprocessor>>` on the given `&BinaryObject` and return a
+/// `HashMap<String, HashMap<String>>`. While concurrency is not yet available in ArmorLib, it
+/// will be implemented in this function, if anywhere.
+///
+/// In _nearly all_ cases, it is better to perform `File.process()`, `Vec<u8>.process()`, or even
+/// `coordinator::process()` than to use this function. The previous functions will make sure
+/// that everything is set up properly; using this function alone will require you to manage
+/// the preprocessors and later scanning yourself. If you need fine grained control, use this
+/// function. Otherwise, don't—you'll save yourself a headache.
+///
+/// # Arguments
+/// * `preprocessors`: a `Vec<Box<preprocessors>>` of the preprocessors to be run.
+/// * `binary_object`: a reference to a `BinaryObject` on which the preprocessors will run.
 pub fn process(
     preprocessors: Vec<Box<Preprocessor>>,
     binary_object: &BinaryObject,
