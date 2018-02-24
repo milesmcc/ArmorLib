e8cbcee4ed60acdcb2986178e18875d657d13d5f
Miles McCain <milesmcc@users.noreply.github.com>
2018-02-24 00:47:44 -0500

    Update auto-generated documentation

--- docs/doc/implementors/core/ops/arith/trait.Mul.js
+++ docs/doc/implementors/core/ops/arith/trait.Mul.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["crypto"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a> for <a class=\"struct\" href=\"crypto/curve25519/struct.Fe.html\" title=\"struct crypto::curve25519::Fe\">Fe</a>",];
 implementors["time"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i32.html\">i32</a>&gt; for <a class=\"struct\" href=\"time/struct.Duration.html\" title=\"struct time::Duration\">Duration</a>",];
 
             if (window.register_implementors) {
